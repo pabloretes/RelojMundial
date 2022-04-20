94c2ec88c9765593db973424d9d86f707e3c6f90
Pablo Retes <Pablo.Retes@gmail.com>
2022-04-19 23:56:33 -0300

    Incorporar eventos

--- js/javascript.js
+++ js/javascript.js
@@ -3,15 +3,13 @@ const difLim = -2;
 const difTel = 6;
 const difVig = 5;
 
-
-
 // Objeto ciudades
 const miArrayCiudades = [
   { idCiudad: 1, ciudad: "Tel Aviv", pais: "Israel" },
-  { idCiudad: 2, ciudad: "Buenos Aires", pais: "Argentina"},
+  { idCiudad: 2, ciudad: "Buenos Aires", pais: "Argentina" },
   { idCiudad: 3, ciudad: "Bogotá", pais: "Colombia" },
   { idCiudad: 4, ciudad: "Lima", pais: "Peru" },
-  { idCiudad: 5, ciudad: "Vigo", pais: "España" }
+  { idCiudad: 5, ciudad: "Vigo", pais: "España" },
 ];
 let date = new Date();
 
@@ -23,24 +21,27 @@ const options = {
   day: "numeric",
 };
 
+// Evento Onclick para la entrega del 19/04
+let boton = document.querySelector("#btnRelojes");
+boton.onclick = fn_actualizarHora();
 
 alert("Bienvenido a Reloj Mundial. A continuación elije tu ciudad");
 let textoEntrada = prompt(
-  "Elige tu ciudad: 1 Tel Aviv | 2 Buenos Aires | 3 Bogotá | 4 Lima | 5 Vigo"
+  "Elige tu ciudad --> | 1 Tel Aviv | 2 Buenos Aires | 3 Bogotá | 4 Lima | 5 Vigo |"
 );
 
-if (textoEntrada <=  miArrayCiudades.length) {
+if (textoEntrada <= miArrayCiudades.length) {
   let tuCiudad = document.getElementById("id_tuCiudad");
   tuCiudad.innerText = devuelveCiudad(textoEntrada);
+  fn_actualizarHora();
   textoEntrada = "ESC";
 } else {
   alert("Error");
   textoEntrada = "ESC";
 }
 
-// Busqueda de la ciudad 
+// Busqueda de la ciudad
 function devuelveCiudad(idCiudad) {
-
   for (let laCiudad of miArrayCiudades) {
     if (idCiudad == laCiudad.idCiudad) {
       return "Tu ciudad es " + laCiudad.ciudad + " - " + laCiudad.pais;
@@ -48,53 +49,57 @@ function devuelveCiudad(idCiudad) {
   }
 }
 
-id_textoCantidadRelojes.textContent = "Reloj Mundial posee " + miArrayCiudades.length + " Relojes";
+id_textoCantidadRelojes.textContent =
+  "Reloj Mundial posee " + miArrayCiudades.length + " Relojes";
 
 
-/* Tel Aviv 6 horas de diferencia    */
-date = new Date();
-date.setHours(date.getHours() + difTel);
+function fn_actualizarHora() {
+  /* Tel Aviv 6 horas de diferencia    */
+  date = new Date();
+  date.setHours(date.getHours() + difTel);
 
-let hora1 = document.getElementById("id_hora1");
-hora1.innerText = date.toLocaleTimeString();
+  let hora1 = document.getElementById("id_hora1");
+  hora1.innerText = date.toLocaleTimeString();
 
-let fecha1 = document.getElementById("id_fecha1");
-fecha1.innerText = date.toLocaleDateString("es-AR", options);
 
-/* Buenos Aires */
-date = new Date();
-let hora2 = document.getElementById("id_hora2");
-hora2.innerText = date.toLocaleTimeString();
+  let fecha1 = document.getElementById("id_fecha1");
+  fecha1.innerText = date.toLocaleDateString("es-AR", options);
 
-let fecha2 = document.getElementById("id_fecha2");
-fecha2.innerText = date.toLocaleDateString("es-AR", options);
+  /* Buenos Aires */
+  date = new Date();
+  let hora2 = document.getElementById("id_hora2");
+  hora2.innerText = date.toLocaleTimeString();
 
-/* Colombia -2 horas de diferencia   */
-date = new Date();
-date.setHours(date.getHours() + difBog);
+  let fecha2 = document.getElementById("id_fecha2");
+  fecha2.innerText = date.toLocaleDateString("es-AR", options);
 
-let hora3 = document.getElementById("id_hora3");
-hora3.innerText = date.toLocaleTimeString();
+  /* Colombia -2 horas de diferencia   */
+  date = new Date();
+  date.setHours(date.getHours() + difBog);
 
-let fecha3 = document.getElementById("id_fecha3");
-fecha3.innerText = date.toLocaleDateString("es-AR", options);
+  let hora3 = document.getElementById("id_hora3");
+  hora3.innerText = date.toLocaleTimeString();
 
-/* Perú -2 horas de diferencia   */
-date = new Date();
-date.setHours(date.getHours() + difLim);
+  let fecha3 = document.getElementById("id_fecha3");
+  fecha3.innerText = date.toLocaleDateString("es-AR", options);
 
-let hora4 = document.getElementById("id_hora4");
-hora4.innerText = date.toLocaleTimeString();
+  /* Perú -2 horas de diferencia   */
+  date = new Date();
+  date.setHours(date.getHours() + difLim);
 
-let fecha4 = document.getElementById("id_fecha4");
-fecha4.innerText = date.toLocaleDateString("es-AR", options);
+  let hora4 = document.getElementById("id_hora4");
+  hora4.innerText = date.toLocaleTimeString();
 
-/* Vigo 5 horas de diferencia    */
-date = new Date();
-date.setHours(date.getHours() + difVig);
+  let fecha4 = document.getElementById("id_fecha4");
+  fecha4.innerText = date.toLocaleDateString("es-AR", options);
 
-let hora5 = document.getElementById("id_hora5");
-hora5.innerText = date.toLocaleTimeString();
+  /* Vigo 5 horas de diferencia    */
+  date = new Date();
+  date.setHours(date.getHours() + difVig);
 
-let fecha5 = document.getElementById("id_fecha5");
-fecha5.innerText = date.toLocaleDateString("es-AR", options);
+  let hora5 = document.getElementById("id_hora5");
+  hora5.innerText = date.toLocaleTimeString();
+
+  let fecha5 = document.getElementById("id_fecha5");
+  fecha5.innerText = date.toLocaleDateString("es-AR", options);
+}
